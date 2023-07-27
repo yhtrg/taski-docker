067e5c8a8aa4d3b2b31d485c351fe765b7623f9f
yhtrg <Ever-Kartashyan1234@yandex.ru>
2023-07-27 09:59:33 +0000

    12

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,11 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    os.getenv('SECRET_KEY', 'GC+XWe0eN/yfeE0TyuFXHlaYSeKlxOyNZt')
-)
+SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_('
+		'^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
